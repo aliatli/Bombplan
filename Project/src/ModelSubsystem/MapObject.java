f7c4e9673dc16a93ad00b8184454ecf3d4846486
Berk Yurttas <yurttasberk@gmail.com>
2016-04-26 23:10:35 +0300

    merge

--- Project/src/ModelSubsystem/MapObject.java
+++ Project/src/ModelSubsystem/MapObject.java
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-package ModelSubsystem;
-
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-public class MapObject {
-
-	public BufferedImage icon;
-	public boolean destroyable;
-	public int x;
-	public int y;
-
-	public boolean isDestroyable() {
-		return this.destroyable;
-	}
-
-	public void draw(Graphics g) {
-		g.drawImage(icon, this.getX(), this.getY(), 64, 64, null);
-	}
-
-	public int getX() {
-		return this.x;
-	}
-
-	public int getY() {
-		return this.y;
-	}
-
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public void setY(int y) {
-		this.y = y;
-	}
-	public void getIconFromFile(String path){
-		try {
-			File img = new File(path);
-			icon = ImageIO.read(img);
-			//System.out.println(icon);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-=======
 package ModelSubsystem;
 
 import java.awt.*;
@@ -100,5 +50,4 @@ public class MapObject {
 		icon.paintIcon(null, g, this.getY()*64, this.getX()*64);
 		//g.drawImage(icon, this.getX(), this.getY(), 64, 64, null);
 	}
->>>>>>> master
 }
--- Project/src/ModelSubsystem/Player.java
+++ Project/src/ModelSubsystem/Player.java
@@ -1,64 +1,3 @@
-<<<<<<< HEAD
-package ModelSubsystem;
-
-//import ControllerSubsystem.*;
-
-import java.awt.image.BufferedImage;
-
-public class Player extends MapObject implements Movable{
-
-	private boolean multipleBomb = false;
-	private int lives = 3;
-	private boolean bombControllable;
-	private int velocity;
-    //private CollisionManager colMan;
-
-	public boolean isMultipleBomb() {
-		return this.multipleBomb;
-	}
-
-	public void setMultipleBomb(boolean multipleBomb) {
-		this.multipleBomb = multipleBomb;
-	}
-
-	/**
-	 * 
-	 * @param type
-	 */
-	public void takeBonus(int type) {
-		// TODO - implement Player.takeBonus
-		throw new UnsupportedOperationException();
-	}
-
-	public boolean isBombControllable() {
-		return this.bombControllable;
-	}
-
-	public Player() {
-		getIconFromFile("player.png");
-	}
-
-	public int getVelocity() {
-		return this.velocity;
-	}
-
-	public int getLife() {
-		return this.lives;
-	}
-
-	public void decreaseLife() {
-		this.lives--;
-	}
-
-	public void move(int movement){
-        if (movement == 0) this.setX(getX()+1);
-        else if (movement == 1) this.setY(getY()+1);
-        else if (movement == 2) this.setX(getX()-1);
-        else if (movement == 3) this.setY(getY()-1);
-
-	}
-
-=======
 package ModelSubsystem;
 
 import java.util.Random;
@@ -131,5 +70,4 @@ public class Player extends MapObject implements Movable{
         else if (movement == 2) this.setX(getX()-1);
         else if (movement == 3) this.setY(getY()-1);
 	}
->>>>>>> master
 }
