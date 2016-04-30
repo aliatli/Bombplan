16854d9013bb6ddf3daea200b0c1b4e1072b9e84
Saner Turhaner <san-pi@windowslive.com>
2016-04-30 17:58:21 +0300

    GameOverPanel label Updated

--- Project/src/UserInterfaceSubsystem/GameOverPanel.java
+++ Project/src/UserInterfaceSubsystem/GameOverPanel.java
@@ -32,9 +32,9 @@ public class GameOverPanel extends JPanel
 
         //Label initialized
         title = new JLabel( "Game Over, your score is: " + point);
-        title.setSize(new Dimension(500,40));
-        title.setLocation(212,325);
-        title.setFont(new Font("Calibri", Font.PLAIN + Font.BOLD, 30));
+        title.setSize(new Dimension(500,50));
+        title.setLocation(232,400);
+        title.setFont(new Font("Calibri", Font.PLAIN + Font.BOLD, 40));
         title.setForeground(new Color(207,54,30));
         title.setVisible(true);
 
